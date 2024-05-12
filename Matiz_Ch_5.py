82a933ae5d0eb86d93866a01968c45fa5f82be18
Rompetr78 <rompetr@yahoo.com>
2024-05-12 19:32:16 +0800

    Added the 5.1 Task

---
+++ Matiz_Ch_5.py
@@ -0,0 +1,28 @@
+"""
+# cars = ['audi', 'bmw', 'subaru', 'toyota']
+#
+# for car in cars:
+#     if car == 'bmw':
+#         print(car.upper())
+#     else:
+#         print(car.title())
+# --------------------------------
+banned_users = ['andrew', 'carolina', 'david']
+user = 'marie'
+
+if user not in banned_users:
+    print(f"{user.title()}, you can post a response if you wish.")
+# --------------------------------
+# 5.1
+car = 'subaru'
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+
+print("\nIs car == 'audi'? I predict False.")
+print(car == 'audi')
+
+print("\nIs 2+3 == 5?")
+print(2 + 3 == 5)
+print("\nIs 5+6 > 7+7?")
+print(5+6 > 7+7 )
+"""
