df673894c809083277b0dd6940e9557a1bb45262
Rompetr78 <rompetr@yahoo.com>
2024-05-29 01:03:30 +0800

    added the aliens in Matiz_Ch_6.py

--- Matiz_Ch_6.py
+++ Matiz_Ch_6.py
@@ -145,3 +145,24 @@ for value in rivers_1.values():
     print(value.title())
 #--------------------------------------
 """
+# Создание пустого списка для хранения пришельцев
+aliens = []
+# Создание 30ти зеленых пришельцев
+for alien_number in range(30):
+    new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
+    aliens.append(new_alien)
+
+# Изменение характеристик первых трех пришельцев
+for alien in aliens[0:3]:
+    if alien['color'] == 'green':
+        alien['color'] = 'yellow'
+        alien['speed'] = 'medium'
+        alien['points'] = 10
+
+# Вывод первых 5ти пришельцев
+for alien in aliens[ :5]:
+     print(alien)
+print("...")
+
+# Вывод количества созданных пришельцев
+print(f"Total number of aliens: {len(aliens)}.")
