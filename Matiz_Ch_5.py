5adf62fd840758c66eb480935d0ea901f1430aa3
Rompetr78 <rompetr@yahoo.com>
2024-05-29 23:00:29 +0800

    added the HW 5.2 in Matiz_Ch_5.py

--- Matiz_Ch_5.py
+++ Matiz_Ch_5.py
@@ -6,21 +6,23 @@
 #         print(car.upper())
 #     else:
 #         print(car.title())
-# --------------------------------
-banned_users = ['andrew', 'carolina', 'david']
-user = 'marie'
 
-if user not in banned_users:
-    print(f"{user.title()}, you can post a response if you wish.")
 # -------------------------------------
-
 requested_toppings = ['mushrooms', 'onions', 'pineapple']
 if 'mushrooms' in requested_toppings:
     print('YES')
 else:
     print('NO')
+
 # -------------------------------------
-'''
+banned_users = ['andrew', 'carolina', 'david']
+user = 'marie'
+
+if user not in banned_users:
+    print(f"{user.title()}, you can post a response if you wish.")
+
+# -------------------------------------
+
 # HW 5.1
 car = 'subaru'
 print("Is car == 'subaru'? I predict True.")
@@ -33,4 +35,21 @@ print("\nIs 2+3 == 5?")
 print(2 + 3 == 5)
 print("\nIs 5+6 > 7+7?")
 print(5+6 > 7+7 )
-'''
+
+#--------------------------------------
+# HW 5.2
+str_1 = 'abc'
+str_2 = 'bca'
+str_3 = 'AbC'
+list_1 = ['a', 'b', 'c', 'd']
+# print(str_1 == str_2)
+# print(str_1 == str_3.lower())
+# print(45 > 34)
+# print(67>=76)
+if 's' in list_1:
+    print('Yes')
+else:
+    print('No')
+
+#--------------------------------------
+"""
