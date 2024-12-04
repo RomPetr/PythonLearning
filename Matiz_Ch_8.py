6f0079cf299aa8180ea8049c3470234853a022b7
Rompetr78 <rompetr@yahoo.com>
2024-12-04 13:40:52 +0800

    added HW 8.4 in Matiz_Ch_8.py

--- Matiz_Ch_8.py
+++ Matiz_Ch_8.py
@@ -1,7 +1,23 @@
 """
+# Позиционные аргументы
 def describe_pet(animal_type, pet_name):
     print(f"\nI have a {animal_type}. ")
     print(f"My {animal_type}'s name is {pet_name.title()}.")
 
 describe_pet('hamster', 'harry')
 """
+# -------------------------------------
+"""
+# Именованные аргументы
+def describe_pet(animal_type, pet_name):
+    print(f"\nI have a {animal_type}. ")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+describe_pet(animal_type='hamster', pet_name='harry')
+"""
+# -------------------------------------
+# HW 8.4
+def make_shirt(size='L', text='I love Python'):
+    print(f"My shirt is size {size}, on it next text '{text}'.")
+
+make_shirt('M', 'I love C')
