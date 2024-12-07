a2267bd8f286081862618c2f2a663f6a3dadcf85
Rompetr78 <rompetr@yahoo.com>
2024-12-08 01:08:18 +0800

    added else 'import pseudonym for module' in Matiz_Ch_8_Making_Pizzas.py

--- Matiz_Ch_8_Making_Pizzas.py
+++ Matiz_Ch_8_Making_Pizzas.py
@@ -11,7 +11,14 @@ from Matiz_Ch_8_Pizza import make_pizza
 make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
 """
 # -------------------------------------
+"""
 # Назначение псевдонима для функции
 from Matiz_Ch_8_Pizza import make_pizza as mp
 
 mp(16, 'pepperoni')
+"""
+# -------------------------------------
+# Назначение псевдонима для модуля
+import Matiz_Ch_8_Pizza as p
+
+p.make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
