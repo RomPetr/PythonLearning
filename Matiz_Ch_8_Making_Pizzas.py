a906db6cd33b4c11db0ebe7b7394526a08ac3622
Rompetr78 <rompetr@yahoo.com>
2024-12-08 00:59:22 +0800

    added some code in Matiz_Ch_8_Making_Pizzas.py

--- Matiz_Ch_8_Making_Pizzas.py
+++ Matiz_Ch_8_Making_Pizzas.py
@@ -1,3 +1,9 @@
+"""
 import Matiz_Ch_8_Pizza
 
 Matiz_Ch_8_Pizza.make_pizza(16, 'pepperoni')
+"""
+
+from Matiz_Ch_8_Pizza import make_pizza
+
+make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
