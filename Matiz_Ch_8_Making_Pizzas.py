d4c9418a620c4ed699af2dff3eb8004ada3cc247
Rompetr78 <rompetr@yahoo.com>
2024-12-08 01:02:39 +0800

    added else some code in Matiz_Ch_8_Making_Pizzas.py

--- Matiz_Ch_8_Making_Pizzas.py
+++ Matiz_Ch_8_Making_Pizzas.py
@@ -3,7 +3,15 @@ import Matiz_Ch_8_Pizza
 
 Matiz_Ch_8_Pizza.make_pizza(16, 'pepperoni')
 """
-
+# -------------------------------------
+"""
+# Импортирование конкретных функций
 from Matiz_Ch_8_Pizza import make_pizza
 
 make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+"""
+# -------------------------------------
+# Назначение псевдонима для функции
+from Matiz_Ch_8_Pizza import make_pizza as mp
+
+mp(16, 'pepperoni')
