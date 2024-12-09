54b3e290b52ed2fe9f9762c8b68af8dfd39cc63b
Rompetr78 <rompetr@yahoo.com>
2024-12-09 13:57:28 +0800

    added my_dog and your_dog class instances in Matiz_Ch_9.py

--- Matiz_Ch_9.py
+++ Matiz_Ch_9.py
@@ -3,7 +3,7 @@ class Dog():
 
     def __init__(self, name, age):
         '''Инициализирует атрибуты name и age'''
-        self.name = name
+        self.name = name.title()
         self.age = age
 
     def sit(self):
@@ -15,5 +15,12 @@ class Dog():
         print(f"{self.name} rolled ower!")
 
 my_dog = Dog('willie', 6)
+your_dog = Dog('lucy', 3)
+
 print(f"My dog's name is {my_dog.name}.")
 print(f"My dog is {my_dog.age} years old.")
+my_dog.sit()
+
+print(f"Your dog's name is {your_dog.name}.")
+print(f"Your dog is {your_dog.age} years old.")
+your_dog.sit()
