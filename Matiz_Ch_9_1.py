999353661ec506dc94dd5a1a6ba87ee40ef78eba
Rompetr78 <rompetr@yahoo.com>
2024-12-09 22:52:25 +0800

    added HW 9.4 in Matiz_Ch_9_1.py

--- Matiz_Ch_9_1.py
+++ Matiz_Ch_9_1.py
@@ -43,7 +43,6 @@ my_new_car.update_odometer(15)
 my_new_car.read_odometer()
 my_new_car.update_odometer(18)
 my_new_car.read_odometer()
-"""
 
 my_used_car = Car('subaru', 'outback', 2015)
 print(my_used_car.get_descriptive_name())
@@ -53,3 +52,36 @@ my_used_car.read_odometer()
 
 my_used_car.increment_odometer(100)
 my_used_car.read_odometer()
+"""
+# -------------------------------------
+# HW 9.4
+class Restaurant():
+
+    def __init__(self, restaurant_name, cuisine_type):
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+        self.number_served = 0
+
+    def describe_restaurant(self):
+        print(f"The name of restaurant is '{self.restaurant_name}'")
+        print(f"The cuisine of this restaurant is {self.cuisine_type}\n")
+
+    def open_restaurant(self):
+        print(f"I would like to inform you that the restaurant "
+              f"'{self.restaurant_name}' is open for visiting from 14-00 to 02-00")
+
+    def set_number_served(self, number):
+        self.number_served = number
+
+    def increment_number_served(self, increment):
+        self.number_served += increment
+
+
+restaurant = Restaurant('HopHop', 'Bulgarian')
+print(restaurant.number_served)
+restaurant.number_served = 12
+print(restaurant.number_served)
+restaurant.set_number_served(23)
+print(restaurant.number_served)
+restaurant.increment_number_served(30)
+print(restaurant.number_served)
