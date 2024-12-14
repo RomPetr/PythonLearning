327ec9e76e1a327f43ba47ca9f55077e2434a4b4
Rompetr78 <rompetr@yahoo.com>
2024-12-14 22:06:10 +0800

    Added learning_python.txt and HW 10.1, HW 10.2 in Matiz_Ch_10.py

--- Matiz_Ch_10.py
+++ Matiz_Ch_10.py
@@ -31,6 +31,7 @@ print(pi_string)
 print(len(pi_string))
 """
 # -------------------------------------
+"""
 # Большие файлы: миллион цифр
 filename = 'pi_million_digits.txt'
 with open(filename) as file_object:
@@ -42,3 +43,54 @@ for line in lines:
 
 print(f"{pi_string[:52]}")
 print(len(pi_string))
+"""
+# -------------------------------------
+# Проверка дня рождения
+"""
+filename = 'pi_million_digits.txt'
+with open(filename) as file_object:
+    lines = file_object.readlines()
+
+pi_string = ''
+for line in lines:
+    pi_string += line.strip()
+
+birthday = input("Enter your birthday, in the form mmddyy: ")
+if birthday in pi_string:
+    print("Your birthday appears in the first million digits of pi!")
+else:
+    print("Your birthday does not appears in the first million digits of pi!")
+"""
+# --------------------------------
+"""
+# HW 10.1
+filname = 'learning_python.txt'
+with open(filname) as file_object:
+    '''
+    # 1
+    contents = file_object.read()
+print(contents.rstrip())
+    '''
+    '''
+    # 2
+    lines = file_object.readlines()
+for line in lines:
+    print(line.rstrip())
+    '''
+    '''
+    # 3
+    lines = file_object.readlines()
+content = ""
+for line in lines:
+    content += line
+print(content)
+'''
+"""
+# -------------------------------------
+# HW 10.2
+filname = 'learning_python.txt'
+with open(filname) as file_object:
+    lines = file_object.readlines()
+for line in lines:
+    line = line.replace('Python', 'C')
+    print(line.rstrip())
