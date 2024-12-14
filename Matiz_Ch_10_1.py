5b480783fdfe863f08022bae88fdc7aee0634e00
Rompetr78 <rompetr@yahoo.com>
2024-12-15 00:42:48 +0800

    Added alice.txt, little_women.txt, moby_dick.txt and try-except in Matiz_Ch_10_1.py

--- Matiz_Ch_10_1.py
+++ Matiz_Ch_10_1.py
@@ -16,6 +16,7 @@ with open('guest.txt', 'w') as f:
     f.write(name)
 """
 # -------------------------------------
+"""
 # HW 10.4
 with open('guest_book.txt', 'a') as file:
     while(True):
@@ -24,3 +25,36 @@ with open('guest_book.txt', 'a') as file:
             break
         greeting = f'Hello, {name}\n'
         file.write(greeting.title())
+"""
+# -------------------------------------
+"""
+# division_calculator
+print("Give me two numbers, and I'll divide them.")
+print("Enter 'q' to quit.")
+
+while(True):
+    first_number = input("First number: ")
+    if first_number == 'q':
+        break
+    second_number = input("Second number: ")
+    if second_number == 'q':
+        break
+    try:
+        answer = int(first_number) / int(second_number)
+    except ZeroDivisionError:
+        print("You can't divide by zero!")
+    else:
+        print(answer)
+"""
+# -------------------------------------
+filename = 'alice.txt'
+try:
+    with open(filename, encoding='utf-8') as f:
+        contents = f.read()
+except FileNotFoundError:
+    print(f"Sorry, the file {filename} does not exist.")
+else:
+    # Подсчет приблизительного количества слов в файле.
+    words = contents.split()
+    num_words = len(words)
+    print(f"The file {filename} has about {num_words} words.")
