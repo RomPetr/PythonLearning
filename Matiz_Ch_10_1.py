02bdc8942518718ce5ae48cdc9c09b1167cfa364
Rompetr78 <rompetr@yahoo.com>
2024-12-14 23:59:18 +0800

    Added programming.txt, guest.txt, guest_book.txt and Matiz_Ch_10_1.py with HW 10.3, HW 10.4

---
+++ Matiz_Ch_10_1.py
@@ -0,0 +1,26 @@
+# Запись в файл
+"""
+filename = 'programming.txt'
+
+with open(filename, 'a') as file_object:
+    file_object.write("I love programming.\n")
+    file_object.write("I love creating new games.\n")
+    file_object.write("I also love finding meaning in large datasets.\n")
+    file_object.write("I love creating apps that can run in a browser.\n")
+"""
+# -------------------------------------
+"""
+# HW 10.3
+name = input("Please, enter your name: ")
+with open('guest.txt', 'w') as f:
+    f.write(name)
+"""
+# -------------------------------------
+# HW 10.4
+with open('guest_book.txt', 'a') as file:
+    while(True):
+        name = input("Please, enter your name, for quit type 'q': ")
+        if name.lower() == 'q':
+            break
+        greeting = f'Hello, {name}\n'
+        file.write(greeting.title())
