5d077ef6830ad2478e71acca4cb49dc1db7da641
Rompetr78 <rompetr@yahoo.com>
2024-12-16 00:55:56 +0800

    added HW 10.6 and HW 10.7 in Matiz_Ch_10_2.py

--- Matiz_Ch_10_2.py
+++ Matiz_Ch_10_2.py
@@ -1,3 +1,4 @@
+"""
 # Работа с несколькими файлами
 def count_words(filename):
     try:
@@ -14,3 +15,39 @@ def count_words(filename):
 filenames = ['alice.txt', 'siddhartha.txt', 'moby_dick.txt', 'little_women.txt']
 for filename in filenames:
     count_words(filename)
+"""
+# -------------------------------------
+"""
+# HW 10.6
+n1 = input("Enter the first number: ")
+try:
+    n1 = int(n1)
+except ValueError:
+    print("This is not number!")
+else:
+    n2 = input("Enter the second number: ")
+    try:
+        n2 = int(n2)
+    except ValueError:
+        print("This is not number!")
+    else:
+        print(f"Summ of {n1} and {n2} is {n1+n2}")
+"""
+# -------------------------------------
+# HW 10.7
+while(True):
+    n1 = input("Enter the first number (for quit, enter 'q'): ")
+    if n1.lower() == 'q':
+        break
+    try:
+        n1 = int(n1)
+    except ValueError:
+        print("This is not number!")
+    else:
+        n2 = input("Enter the second number: ")
+        try:
+            n2 = int(n2)
+        except ValueError:
+            print("This is not number!")
+        else:
+            print(f"Summ of {n1} and {n2} is {n1+n2}")
