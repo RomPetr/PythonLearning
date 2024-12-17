e3e8bacc4a348a6faba9f183a7d3e269b5ff5344
Rompetr78 <rompetr@yahoo.com>
2024-12-17 20:05:57 +0800

    added HW 10.13 in Matiz_Ch_10_3.py

--- Matiz_Ch_10_3.py
+++ Matiz_Ch_10_3.py
@@ -79,6 +79,7 @@ with open(filename) as f:
 print(f"I know your favorite number! It is {number}")
 """
 # -------------------------------------
+"""
 # HW 10.12
 filename = 'favorite_number.json'
 try:
@@ -91,3 +92,40 @@ except FileNotFoundError:
         print(f"We'll remember your favorite number when you come back, {number}!")
 else:
     print(f"Your favorite number is {number}!")
+"""
+# -------------------------------------
+# HW 10.13
+def get_stored_username():
+    ''' Получает хранимое имя пользователя, если оно существует '''
+    filename = 'username.json'
+    try:
+        with open(filename) as f:
+            username = json.load(f)
+    except FileNotFoundError:
+        return None
+    else:
+        return username
+
+def get_new_username():
+    ''' Запрашивает новое имя пользователя '''
+    username = input("What is your name? ")
+    filename = 'username.json'
+    with open(filename, 'w') as f:
+        json.dump(username, f)
+    return username
+
+def greet_user():
+    ''' Приветствует пользователя по имени '''
+    username = get_stored_username()
+    if username:
+        answer = input(f"It is your name - {username}? (enter 'y' or 'n') ")
+        if answer.lower() == 'y':
+            print(f"Welcome back, {username}!")
+        else:
+            username = get_new_username()
+            print(f"Welcome, {username}!")
+    else:
+        username = get_new_username()
+        print(f"We'll remember you when you come back, {username}!")
+
+greet_user()
